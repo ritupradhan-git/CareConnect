857ab58c3ed05f2c7ba9768ce6edc6ea6ba1a5a0
Rituparna Pradhan <rituppradhan2004@gmail.com>
2026-02-10 15:33:53 +0530

    Added admin auth and updated admin Route

---
+++ server/middleware/authAdmin.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+//Admin authentication middleware
+
+const authAdmin=async(req,res,next)=>{
+    try {
+        const {atoken}=req.headers;
+        if(!atoken){
+            return res.json({success:false,message:'Not authorized Login again.'})
+
+        }
+        const tokenDecode=jwt.verify(atoken,process.env.JWT_SECRET);
+        if(tokenDecode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
+            return res.json({success:false,message:'Not authorized Login again.'})
+        }
+        next()
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});
+    }
+}
+export default authAdmin
--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { addDoctor, loginAdmin } from '../controllers/adminController.js';
 import upload from '../middleware/multer.js';
+import authAdmin from '../middleware/authAdmin.js';
 
 const adminRouter=express.Router();//Creates a mini app for admin routes
 //Sends a post req with a milderware of multer for image upload
-adminRouter.post('/add-doctor',upload.single('image'),addDoctor);
+adminRouter.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
 
 //Login Route
 adminRouter.post('/login',loginAdmin);
