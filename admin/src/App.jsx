b90197be236a6d0ad1848e8f73edc4eb0cfea109
Rituparna Pradhan <rituppradhan2004@gmail.com>
2026-02-12 16:24:38 +0530

    Update admin dashboard with navbar and sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,11 +3,29 @@ import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from './context/AdminContext';
+import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointment from './pages/Admin/AllAppointment';
+import AddDoctor from './pages/Admin/AddDoctor';
+import DoctorsList from './pages/Admin/DoctorsList';
 const App = () => {
   const {aToken}=useContext(AdminContext)
   return aToken ? (
-    <div>
+    <div className='bg-[#f8f9fd]'>
       <ToastContainer/>
+      <NavBar/>
+      <div className='flex items-start'>
+        <SideBar/>
+        <Routes>
+          <Route path='/' element={ <></>}/>
+          <Route path='/admin-dashboard' element={ <Dashboard/>}/>
+          <Route path='/all-appointments' element={ <AllAppointment/>}/>
+          <Route path='/add-doctor' element={ <AddDoctor/>}/>
+          <Route path='/doctor-list' element={ <DoctorsList/>}/>
+        </Routes>
+      </div>
     </div>
   ): (
     <>
--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useState } from "react";
 export const AdminContext=createContext();//global box for storing data
 
 const AdminContextProvider=(props)=>{ //wrapper component-wraps the whole app
-    const [aToken,setAToken]=useState('');
+    const [aToken,setAToken]=useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'');
     const backendUrl=import.meta.env.VITE_BACKEND_URL
     const value={//data stored globally
         aToken,
