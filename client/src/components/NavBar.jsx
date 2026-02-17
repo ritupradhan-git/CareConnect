39f3da41454ea1bf64af2bff87d3fc6746816344
Rituparna Pradhan <rituppradhan2004@gmail.com>
2026-02-18 00:35:12 +0530

    Updated the logo

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
     }
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 '>
-      <img onClick={()=>navigate(`/`)} src={assets.logo} alt="" className='w-44 cursor-pointer'/>
+      <img onClick={()=>navigate(`/`)} src={assets.logo} alt="" className='h-10 w-auto cursor-pointer'/>
       <ul className='hidden md:flex items-start gap-5 font-medium'>
         <NavLink to={'/'}>
             <li className='py-1 '>Home</li>
